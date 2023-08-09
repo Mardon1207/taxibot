bf84de543208f01b90c82a5a6d4da8960cb0e5d8
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-09 11:45:55 +0500

    asosiy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telegram import Update,InlineKeyboardButton,InlineKeyboardMarkup,KeyboardBu
 from telegram.ext import Updater,CommandHandler,CallbackContext,Filters,MessageHandler,CallbackQueryHandler
 import json
 import os
+havola="https://t.me/"
 # TOKEN=os.environ.get("TOKEN")
 TOKEN="5850794189:AAFnFdgE9wJ70__uodPKrR6yaIcv_anIu60"
 from db import DB
@@ -42,12 +43,23 @@ def elonqosh(update:Update,context:CallbackContext):
         text="Sizni e'loningiz mavjud!"
         query.answer("Sizni e'loningiz mavjud!")
 
+def contact(update:Update,context:CallbackContext):
+    chat_id=update.message.chat.id
+    bot=context.bot
+    query=update.callback_query
+    button = KeyboardButton("Telefon raqamni yuborish", request_contact=True)
+    keyboard = ReplyKeyboardMarkup([[button]],one_time_keyboard=True, resize_keyboard=True)
+    bot.send_message(chat_id, "Iltimos, telefon raqamingizni yuboring:", reply_markup=keyboard)
+
 def addism(update:Update,context:CallbackContext):
     query=update.callback_query
     bot=context.bot
+    phone=update.message.contact.phone_number
+    user=update.message.chat.username
+    print(user)
     chat_id=update.message.chat.id
     ism=update.message.text
-    olsa=InlineKeyboardButton(text="Oltinsoydan Samarqandga",callback_data=f"oltinsam,{chat_id},{ism}")
+    olsa=InlineKeyboardButton(text="Oltinsoydan Samarqandga",callback_data=f"Oltinsoy,Samarqand,{chat_id},{ism},{user},{phone}")
     olnu=InlineKeyboardButton(text="Oltinsoydan Nurobodga",callback_data=f"oltinnur,{chat_id},{ism}")
     olju=InlineKeyboardButton(text="Oltinsoyan Jumaga",callback_data=f"oltinjum,{chat_id},{ism}")
     olto=InlineKeyboardButton(text="Oltinsoyan Toshkentga",callback_data=f"oltintosh,{chat_id},{ism}")
@@ -75,7 +87,8 @@ def qoshish(update:Update,context:CallbackContext):
 dp.add_handler(CommandHandler("start",start))
 dp.add_handler(CallbackQueryHandler(taxi,pattern="taksi"))
 dp.add_handler(CallbackQueryHandler(elonqosh,pattern="elonqoshish"))
-dp.add_handler(MessageHandler(Filters.text,addism))
+dp.add_handler(MessageHandler(Filters.text,contact))
+dp.add_handler(MessageHandler(Filters.contact,addism))
 dp.add_handler(CallbackQueryHandler(qoshish,pattern="oltinsam"))
 updater.start_polling()
 updater.idle()
