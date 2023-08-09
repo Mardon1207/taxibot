f5b421a0ef744b850e2e32e81df5986f6c034f40
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-09 10:42:55 +0500

    data base

---
+++ db.py
@@ -0,0 +1,29 @@
+from tinydb import TinyDB,Query
+from tinydb.database import Document
+
+class DB:
+    def __init__(self,path):
+        self.db = TinyDB(path,indent=4)
+        
+    def save(self,chat_id,ism,qayer):
+        """
+        add card
+
+        data = {
+            'brand':brand,
+            'doc_id': doc_id,
+            chat_id: chat_id
+            }
+        """
+        user=Document(
+            {"ism":ism,
+             "qayer":qayer},doc_id=chat_id)
+        self.db.insert(user)
+
+    def add_ism(self, chat_id,ism):
+        ism={"ism":ism}
+        self.db.update(ism,doc_ids=[chat_id])
+
+    def remove(self, chat_id):
+        user=Query()
+        self.table.remove(user.chat_id==str(chat_id))    
