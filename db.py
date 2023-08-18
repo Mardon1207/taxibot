82ea6474711b281c3a6f4b53f8044aea17f1ee15
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-18 16:03:27 +0500

    db.py

--- db.py
+++ db.py
@@ -4,19 +4,30 @@ from tinydb.database import Document
 class DB:
     def __init__(self,path):
         self.db = TinyDB(path,indent=4)
+        self.table = self.db.table('taxi')
         
-    def save(self,chat_id,ism,phone,user,qayer):
-        users=Document(
-            {"ism":ism,
-             "phone":phone,
-             "user":user,
-             "qayer":qayer},doc_id=chat_id)
-        self.db.insert(users)
+    def save(self,chat_id,ism,phone,havola,qayer,rasm):
+        date={"chat_id":chat_id,
+              "ism":ism,
+              "phone":phone,
+              "havola":havola,
+              "qayer":qayer,
+              "rasm":rasm}
+        self.table.insert(date)
 
     def izla(self, chat_id):
         user=Query()
-        self.db.search(doc_id=chat_id)
+        dic=self.table
+        return dic.search(user.chat_id==chat_id)
+
 
     def remove(self, chat_id):
         user=Query()
-        self.table.remove(user.chat_id==str(chat_id))    
+        self.table.remove(user.chat_id==chat_id)    
+
+    def hamma(self):
+        return self.table.all()
+
+    def manzil(self,qayer):
+        user=Query()
+        return self.table.search(user.qayer==qayer)
